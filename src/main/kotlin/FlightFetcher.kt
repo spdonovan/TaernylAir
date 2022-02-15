d91c3f1f87c6f51d3093879f571451aceea57a68
ScottDonovan <spdonovan@hotmail.co.uk>
2022-02-15 13:35:24 +0000

    using runBlocking and suspend functions

--- src/main/kotlin/FlightFetcher.kt
+++ src/main/kotlin/FlightFetcher.kt
@@ -1,12 +1,21 @@
+import kotlinx.coroutines.*
 import java.net.URL
 
 private const val BASE_URL = "http://kotlin-book.bignerdranch.com/2e"
 private const val FLIGHT_ENDPOINT = "$BASE_URL/flight"
 
 fun main() {
-    val flight = fetchFlight()
-    println(flight)
+    runBlocking {
+        println("Started")
+        launch {
+            val flight = fetchFlight()
+            println(flight)
+        }
+        println("Finished")
+    }
 }
 
-fun fetchFlight(): String = URL(FLIGHT_ENDPOINT).readText()
+suspend fun fetchFlight(): String = withContext(Dispatchers.IO) {
+    URL(FLIGHT_ENDPOINT).readText()
+}
 
